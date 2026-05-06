61e5c9be01662c1442f261611f5fc97b47c2fd07
Tim De Graeve <codemeteor@gmail.com>
2026-05-06 13:50:43 +0200

    🐛 fix(app): fix user permissioons

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu:24.04 AS buildozer
 
-ENV USER="ubuntu"
+ENV USER="user"
 ENV HOME_DIR="/home/${USER}"
 ENV WORK_DIR="${HOME_DIR}/hostcwd" \
     SRC_DIR="${HOME_DIR}/src" \
@@ -25,19 +25,29 @@ RUN apt install -y git zip unzip openjdk-17-jdk python3-pip \
     python3-virtualenv autoconf libtool pkg-config zlib1g-dev \
     libncurses5-dev libncursesw5-dev libtinfo6 cmake libffi-dev \
     libssl-dev automake autopoint gettext \
-    python3.14 python3.14-venv python3.14-dev
+    python3.14 python3.14-venv python3.14-dev \
+    sudo
 
-USER ubuntu
+RUN mkdir -p ${HOME_DIR} \
+    && python3.14 -m venv ${HOME_DIR}/.venv
 
-WORKDIR /home/ubuntu
+WORKDIR ${WORK_DIR}
 RUN mkdir src
+
 RUN curl https://sh.rustup.rs >> rustup.sh
 RUN chmod +x rustup.sh
 RUN ./rustup.sh -y
 RUN rm rustup.sh
 
+ENV VIRTUAL_ENV=${HOME_DIR}/.venv
+ENV PATH="${VIRTUAL_ENV}/bin:${PATH}"
+
 RUN python3.14 -m pip install -U setuptools && \
     python3.14 -m pip install -U git+https://github.com/kivy/buildozer && \
     python3.14 -m pip install -U legacy-cgi cython==0.29.34
 
-WORKDIR /home/ubuntu/src
+COPY --chmod=755 entrypoint.sh /usr/local/bin/entrypoint.sh
+
+WORKDIR ${HOME_DIR}/hostcwd
+
+ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
--- buildozer.sh
+++ buildozer.sh
@@ -7,10 +7,10 @@ if [ ! -x "$(command -v docker)" ]; then
     exit 1
 fi
  
-if [ -z "$(docker images -q realcodemeteor/buildozer-container:v0.1.0 2> /dev/null)" ]; then
+if [ -z "$(docker images -q realcodemeteor/buildozer-container:v0.1.1 2> /dev/null)" ]; then
     echo "Docker image not found!"
     echo "Starting build"
-    docker pull realcodemeteor/buildozer-container:v0.1.0
+    docker pull realcodemeteor/buildozer-container:v0.1.1
 fi
 
 if [ ! -d /tmp/.buildozer ]; then
@@ -18,5 +18,7 @@ if [ ! -d /tmp/.buildozer ]; then
     mkdir /tmp/.buildozer
 fi
 
-docker run --interactive --privileged -v /dev/bus/usb:/dev/bus/usb --tty --rm --volume ${PWD}:/home/ubuntu/src --volume ${HOME}/.android:/home/ubuntu/.android --volume /tmp/.buildozer:/home/ubuntu/.buildozer realcodemeteor/buildozer-container:v0.1.0 buildozer $@
+#docker run --interactive --privileged -v /dev/bus/usb:/dev/bus/usb --tty --rm --volume ${PWD}:/home/user/src --volume ${HOME}/.android:/home/user/.android --volume /tmp/.buildozer:/home/user/.buildozer realcodemeteor/buildozer-container:v0.1.0 buildozer $@
+
+docker run --interactive --privileged -v /dev/bus/usb:/dev/bus/usb --tty --rm --volume ${PWD}:/home/user/src --volume ${HOME}/.android:/home/user/.android --volume /tmp/.buildozer:/home/user/.buildozer --volume ${PWD}:/home/user/hostcwd realcodemeteor/buildozer-container:v0.1.1 $@
 
